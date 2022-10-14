e6ff46b30bb8b2d7e710d64ab61d577c9a13bd83
rjsnhk <114793887+rjsnhk@users.noreply.github.com>
2022-10-14 16:47:31 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,36 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+#print("Hello world")
+#if 6 > 4:
+   # print('six greater than four')
+   #print('hello world')
+#simillar space in the same block you can give 100 spaces
+#for comments use #
+
+#organized variable
+# x=6 #number
+# print(x)
+# y='master'
+# print(y)
+
+#unorganized variable
+# x=10
+# print(x)
+# x='hello world'
+# print(x)
+
+# a=10
+# A='hello world'
+# print(a)
+# print(A)
+# it is not case sensitive
+
+
+#get the type of a variable type()
+# x=20
+# y='raja'
+# z=2.45
+
+# print(type(x))      
+# print(type(y))
+# print(type(z))
