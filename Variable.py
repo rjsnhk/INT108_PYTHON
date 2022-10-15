06a28e949cd842d7e6a9f6572c285ed4e5a8f559
rjsnhk <114793887+rjsnhk@users.noreply.github.com>
2022-10-15 13:37:50 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,56 @@
+#casting in python
+#to specify the data type of a variable use casting
+
+# x=str(10)
+# y=int(10)
+# z=float(10)
+
+# WE CANT ADD STR WITH INT,FLOAT IT WILL SHOW WRONG
+
+# # print(x+y)   wrong
+# # print(x+x)   right
+
+# print(z+y)
+# print(int(z+y))
+# print(type(z+y))
+
+
+#VARIABLE
+# short name (x,y,z))
+# description like (name age total_price)
+
+
+# RULES FOR PYTHON VARIABLE
+# 1 it cant st with numbers
+# 2 it can st with _ or a letter
+# 3 it can contain alpha , numerics , underscores (A-Z , 0-9 _)
+# 4 variable name are case sensitive(name,NAME,Name)
+
+
+# VALID VARIABLE NAME
+# myname='john'
+# print(myname)
+# Myname='john'
+# MYNAME='john'
+# my_name='john'
+# _my_name='john'
+# myname698='john'
+# print(myname698)
+
+# INVALID VARIABLE
+# 3myname='john'
+# my-name='john'
+# my name='john'
+
+# myfirstname='john'
+
+# CAMEL CASE
+# myFirstname='john'
+
+# PASCAL CASE
+# MyFirstName='JOHN'
+
+# SNAKE CASE
+# my_first_name='john'
+
+#use only one case fore a single programming
