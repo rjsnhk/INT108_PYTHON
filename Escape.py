7b91e03e1308d143cbf34a15e1ff11adfc2abcca
rjsnhk <114793887+rjsnhk@users.noreply.github.com>
2022-10-22 11:35:08 +0530

    Add files via upload

---
+++ Escape.py
@@ -0,0 +1,11 @@
+# Python Escape Character = bich me ' " \ use krna ho
+# Code	Result
+# \'	Single Quote
+# \\	Backslash
+# \n	New Line
+# \r	Carriage Return
+# \t	Tab
+# \b	Backspace
+# \f	Form Feed
+# \ooo	Octal value
+# \xhh	Hex value
